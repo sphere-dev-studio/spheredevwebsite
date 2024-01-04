06240c7666391bab8c5e816f359d1c85b90ccfa6
GillyanComo <gillyan.como@epitech.eu>
2024-01-04 18:04:52 +0100

    first test

---
+++ counter.js
@@ -0,0 +1,430 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
+import { FilmPass } from 'three/examples/jsm/postprocessing/FilmPass.js';
+
+
+const canvas = document.querySelector('canvas.webgl')
+
+// Initialisation de Three.js
+const scene = new THREE.Scene()
+
+// Couleur de fond
+const background = "#000000";
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(background, 0);
+document.body.appendChild(renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// Updated color values
+const uC1 = hexToRgb("#" + new THREE.Color(0x181338).getHexString()); // New color #181338
+const uC2 = hexToRgb("#" + new THREE.Color(0xe45500).getHexString()); // New color #e45500
+const uC3 = hexToRgb("#" + new THREE.Color(0xeb4217).getHexString());
+
+
+// Function to convert a hex color to an RGB object
+function hexToRgb(hex) {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16),
+    }
+    : null;
+}
+
+
+
+// Function to format a color value
+function formatColor(color = 0) {
+  return color / 255;
+}
+
+// Création de la sphère
+const geometry = new THREE.SphereGeometry(5, 32, 32);
+
+const uniforms = {
+  uTime: { value: 0.0 },
+  uSpeed: { value: 0.1 },
+  uNoiseDensity: { value: 1 },
+  uNoiseStrength: { value: 0.15 },
+  uFrequency: { value: 3 },
+  uAmplitude: { value: 2.6 },
+  diffuse: { value: new THREE.Color(0x181338) },
+  emissive: { value: new THREE.Color(0x181338) },
+  roughness: { value: 0.5 },
+  metalness: { value: 0.5 },
+  opacity: { value: 1 },
+  reflectivity: { value: 1 },
+  sheen: { value: new THREE.Color(0x181338) },
+};
+
+
+// `;
+
+const vertexShader = `
+
+vec3 mod289(vec3 x)
+{
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec4 mod289(vec4 x)
+{
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec4 permute(vec4 x)
+{
+  return mod289(((x*34.0)+1.0)*x);
+}
+
+vec4 taylorInvSqrt(vec4 r)
+{
+  return 1.79284291400159 - 0.85373472095314 * r;
+}
+
+vec3 fade(vec3 t) {
+  return t*t*t*(t*(t*6.0-15.0)+10.0);
+}
+
+// Classic Perlin noise, periodic variant
+float pnoise(vec3 P, vec3 rep)
+{
+  vec3 Pi0 = mod(floor(P), rep); // Integer part, modulo period
+  vec3 Pi1 = mod(Pi0 + vec3(1.0), rep); // Integer part + 1, mod period
+  Pi0 = mod289(Pi0);
+  Pi1 = mod289(Pi1);
+  vec3 Pf0 = fract(P); // Fractional part for interpolation
+  vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+  vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+  vec4 iy = vec4(Pi0.yy, Pi1.yy);
+  vec4 iz0 = Pi0.zzzz;
+  vec4 iz1 = Pi1.zzzz;
+
+  vec4 ixy = permute(permute(ix) + iy);
+  vec4 ixy0 = permute(ixy + iz0);
+  vec4 ixy1 = permute(ixy + iz1);
+
+  vec4 gx0 = ixy0 * (1.0 / 7.0);
+  vec4 gy0 = fract(floor(gx0) * (1.0 / 7.0)) - 0.5;
+  gx0 = fract(gx0);
+  vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+  vec4 sz0 = step(gz0, vec4(0.0));
+  gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+  gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+
+  vec4 gx1 = ixy1 * (1.0 / 7.0);
+  vec4 gy1 = fract(floor(gx1) * (1.0 / 7.0)) - 0.5;
+  gx1 = fract(gx1);
+  vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+  vec4 sz1 = step(gz1, vec4(0.0));
+  gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+  gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+
+  vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+  vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+  vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+  vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+  vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+  vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+  vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+  vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
+
+  vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+  g000 *= norm0.x;
+  g010 *= norm0.y;
+  g100 *= norm0.z;
+  g110 *= norm0.w;
+  vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+  g001 *= norm1.x;
+  g011 *= norm1.y;
+  g101 *= norm1.z;
+  g111 *= norm1.w;
+
+  float n000 = dot(g000, Pf0);
+  float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+  float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+  float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+  float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+  float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+  float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+  float n111 = dot(g111, Pf1);
+
+  vec3 fade_xyz = fade(Pf0);
+  vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+  vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+  float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x);
+  return 2.2 * n_xyz;
+}
+
+
+//-------- start here ------------
+
+varying vec3 vNormal;
+uniform float uTime;
+uniform float uSpeed;
+uniform float uNoiseDensity;
+uniform float uNoiseStrength;
+uniform float uFrequency;
+uniform float uAmplitude;
+varying vec3 vPos;
+varying float vDistort;
+varying vec2 vUv;
+varying vec3 vViewPosition;
+
+#define STANDARD
+    varying vec3 vTangent;
+    varying vec3 vBitangent;
+
+#include <clipping_planes_pars_vertex>
+#include <color_pars_vertex>
+#include <common>
+#include <displacementmap_pars_vertex>
+#include <fog_pars_vertex>
+#include <logdepthbuf_pars_vertex>
+#include <morphtarget_pars_vertex>
+#include <shadowmap_pars_vertex>
+#include <skinning_pars_vertex>
+// #include <uv2_pars_vertex>
+#include <uv_pars_vertex>
+
+
+// rotation
+mat3 rotation3dY(float angle) {
+  float s = sin(angle);
+  float c = cos(angle);
+  return mat3(c, 0.0, -s, 0.0, 1.0, 0.0, s, 0.0, c);
+}
+
+vec3 rotateY(vec3 v, float angle) { return rotation3dY(angle) * v; }
+
+void main() {
+  #include <beginnormal_vertex>
+  #include <color_vertex>
+  #include <defaultnormal_vertex>
+  #include <morphnormal_vertex>
+  #include <skinbase_vertex>
+  #include <skinnormal_vertex>
+  // #include <uv2_vertex>
+  #include <uv_vertex>
+    vNormal = normalize(transformedNormal);
+  #ifdef USE_TANGENT
+    vTangent = normalize(transformedTangent);
+    vBitangent = normalize(cross(vNormal, vTangent) * tangent.w);
+  #endif
+  #include <begin_vertex>
+
+  #include <clipping_planes_vertex>
+  #include <displacementmap_vertex>
+  #include <logdepthbuf_vertex>
+  #include <morphtarget_vertex>
+  #include <project_vertex>
+  #include <skinning_vertex>
+    vViewPosition = -mvPosition.xyz;
+  #include <fog_vertex>
+  #include <shadowmap_vertex>
+  #include <worldpos_vertex>
+
+  //-------- start vertex ------------
+  float t = uTime * uSpeed;
+  float distortion =
+      pnoise((normal + t) * uNoiseDensity, vec3(10.0)) * uNoiseStrength;
+  vec3 pos = position + (normal * distortion);
+  float angle = sin(uv.y * uFrequency + t) * uAmplitude;
+  pos = rotateY(pos, angle);
+
+  vPos = pos;
+  vDistort = distortion;
+  vNormal = normal;
+  vUv = uv;
+
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.);
+}
+`;
+
+const fragmentShader = `
+#define STANDARD
+#define REFLECTIVITY
+#define CLEARCOAT
+#define TRANSMISSION
+uniform vec3 diffuse;
+uniform vec3 emissive;
+uniform float roughness;
+uniform float metalness;
+uniform float opacity;
+uniform float transmission;
+uniform float reflectivity;
+// uniform float clearcoat;
+// uniform float clearcoatRoughness;
+uniform vec3 sheen;
+varying vec3 vViewPosition;
+varying vec3 vTangent;
+varying vec3 vBitangent;
+#include <alphamap_pars_fragment>
+#include <aomap_pars_fragment>
+#include <color_pars_fragment>
+#include <common>
+#include <dithering_pars_fragment>
+#include <emissivemap_pars_fragment>
+#include <lightmap_pars_fragment>
+#include <map_pars_fragment>
+#include <packing>
+// #include <uv2_pars_fragment>
+#include <uv_pars_fragment>
+// #include <transmissionmap_pars_fragment>
+#include <bsdfs>
+#include <bumpmap_pars_fragment>
+#include <clearcoat_pars_fragment>
+#include <clipping_planes_pars_fragment>
+#include <cube_uv_reflection_fragment>
+#include <envmap_common_pars_fragment>
+#include <envmap_physical_pars_fragment>
+#include <fog_pars_fragment>
+#include <lights_pars_begin>
+#include <lights_physical_pars_fragment>
+#include <logdepthbuf_pars_fragment>
+#include <metalnessmap_pars_fragment>
+#include <normalmap_pars_fragment>
+#include <roughnessmap_pars_fragment>
+#include <shadowmap_pars_fragment>
+varying vec3 vNormal;
+varying float displacement;
+varying vec3 vPos;
+varying float vDistort;
+varying vec3 color1;
+varying vec3 color2;
+varying vec3 color3;
+varying float distanceToCenter;
+
+float linearToRelativeLuminance( const in vec3 color ) {
+
+	vec3 weights = vec3( 0.2126, 0.7152, 0.0722 );
+
+	return dot( weights, color.rgb );
+
+}
+
+void main() {
+  //-------- basic gradient ------------
+  vec3 color1 = vec3(0.,0.251,0.051);
+  vec3 color2 = vec3(0.,0.322,0.322);
+  vec3 color3 = vec3(0.,0.922,0.922);
+  float clearcoat = 1.0;
+  float clearcoatRoughness = 0.5;
+#include <clipping_planes_fragment>
+
+  float distanceToCenter = distance(vPos, vec3(0, 0, 0));
+
+  vec4 diffuseColor =
+      vec4(mix(color3, mix(color2, color1, smoothstep(-1.0, 1.0, vPos.y)),
+               distanceToCenter),
+           1);
+
+  // vec4 diffuseColor = vec4(mix(color1, color2, smoothstep(-1.0, 1.0, vPos.y)), 1);
+
+  //-------- materiality ------------
+  ReflectedLight reflectedLight =
+      ReflectedLight(vec3(0.0), vec3(0.0), vec3(0.0), vec3(0.0));
+  vec3 totalEmissiveRadiance = emissive;
+  float totalTransmission = transmission;
+#include <logdepthbuf_fragment>
+#include <map_fragment>
+#include <color_fragment>
+#include <alphamap_fragment>
+#include <alphatest_fragment>
+#include <roughnessmap_fragment>
+#include <metalnessmap_fragment>
+#include <normal_fragment_begin>
+#include <normal_fragment_maps>
+#include <clearcoat_normal_fragment_begin>
+#include <clearcoat_normal_fragment_maps>
+#include <emissivemap_fragment>
+// #include <transmissionmap_fragment>
+#include <lights_physical_fragment>
+#include <lights_fragment_begin>
+#include <lights_fragment_maps>
+#include <lights_fragment_end>
+#include <aomap_fragment>
+  vec3 outgoingLight =
+      reflectedLight.directDiffuse + reflectedLight.indirectDiffuse +
+      reflectedLight.directSpecular + reflectedLight.indirectSpecular;
+  diffuseColor.a *=
+      mix(saturate(1. - totalTransmission +
+                   linearToRelativeLuminance(reflectedLight.directSpecular +
+                                             reflectedLight.indirectSpecular)),
+          1.0, metalness);
+  gl_FragColor = vec4(outgoingLight, diffuseColor.a);
+
+#include <tonemapping_fragment>
+#include <colorspace_fragment>
+#include <fog_fragment>
+#include <premultiplied_alpha_fragment>
+#include <dithering_fragment>
+}
+`;
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+// Application des Shaders
+const material = new THREE.ShaderMaterial({
+  vertexShader: vertexShader,
+  fragmentShader: fragmentShader,
+  uniforms: uniforms,
+});
+
+
+// Lumière
+
+const directionalLight = new THREE.DirectionalLight('#ffffff', 3)
+const ambientLight = new THREE.AmbientLight('#ffffff', 1)
+directionalLight.position.set(10, 10, 10)
+scene.add(ambientLight)
+scene.add(directionalLight)
+
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+
+// Position de la caméra
+camera.position.z = 10;
+
+// Fonction de rendu
+function animate() {
+  requestAnimationFrame(animate);
+
+  //utime
+  uniforms.uTime.value += 0.01;
+
+  controls.update();
+
+  renderer.render(scene, camera);
+  // composer.render();
+
+}
+
+animate();
