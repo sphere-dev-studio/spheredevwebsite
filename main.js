94b17754d004f59ce2758484b4f5ba4393475bd7
GillyanComo <gillyan.como@epitech.eu>
2024-01-04 20:10:51 +0100

    change color

--- main.js
+++ main.js
@@ -199,7 +199,7 @@ vec3 cosPalette(float t, vec3 a, vec3 b, vec3 c, vec3 d) {
 void main() {
   float distort = vDistort * 2.0;
 
-  vec3 brightness = vec3(0.5, 0.5, 0.5);
+  vec3 brightness = vec3(0.6, 0.5, 0.7);
   vec3 contrast = vec3(0.5, 0.5, 0.5);
   vec3 oscilation = vec3(1.0, 1.0, 1.0);
   vec3 phase = vec3(0.0, 0.1, 0.2);
